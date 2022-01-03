281b474c74beb4623b58607d14620274c9b6f5a2
Nick Pateman <nick@devoctomy.com>
2022-01-03 16:59:50 +0000

    Starting process of generating entire dungeon.

--- DungeonGenerator2d/DungeonGenerator.cs
+++ DungeonGenerator2d/DungeonGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace DungeonGenerator2d
 {
@@ -6,20 +7,23 @@ namespace DungeonGenerator2d
     {
         private readonly IRandomiser _randomiser;
         private readonly IRoomGenerator _roomGenerator;
+        private readonly IArrayTrimmer<BoardTile> _trimmer;
 
         public DungeonGenerator(
             IRandomiser randomiser,
-            IRoomGenerator roomGenerator)
+            IRoomGenerator roomGenerator,
+            IArrayTrimmer<BoardTile> trimmer)
         {
             _randomiser = randomiser;
             _roomGenerator = roomGenerator;
+            _trimmer = trimmer;
         }
 
         public BoardTile[,] Generate(DungeonGeneratorOptions options)
         {
             var board = new BoardTile[
-                options.Width,
-                options.Height];
+                1000,
+                1000];
 
             var rooms = new List<BoardTile[,]?>();
             var startRoomGeneratorOptions = new RoomGeneratorOptions
@@ -30,32 +34,79 @@ namespace DungeonGenerator2d
                 PartWidth = options.RoomPartWidth,
                 PartHeight = options.RoomPartHeight,
                 DoorSize = options.DoorSize,
-                DoorCount = new IntRange(1, 1) //options.RoomDoorCount
+                DoorCount = new IntRange(1, 1),
+                MandatoryDoor = Direction.South
             };
             var startRoom = _roomGenerator.Generate(startRoomGeneratorOptions);
             rooms.Add(startRoom);
+            CopyRoomToBoard(
+                board,
+                startRoom,
+                new Point(500, 500));
 
+            return _trimmer.Trim(board);
+        }
 
+        //private void CreateRoomOffDoor(
+        //    BoardTile[,] board,
+        //    Point doorPos)
+        //{
+        //    var doorDirection = board[]
+        //}
 
-            //var roomCount = _randomiser.GetNext(options.Rooms);
-            //var rooms = new List<BoardTile[,]?>();
-            //while(rooms.Count < roomCount)
-            //{
-            //    var roomGeneratorOptions = new RoomGeneratorOptions
-            //    {
-            //        Width = options.RoomWidth,
-            //        Height = options.RoomHeight,
-            //        Parts = options.RoomParts,
-            //        PartWidth = options.RoomPartWidth,
-            //        PartHeight = options.RoomPartHeight,
-            //        DoorSize = options.DoorSize,
-            //        DoorCount = options.RoomDoorCount
-            //    };
-            //    var room = _roomGenerator.Generate(roomGeneratorOptions);
-            //    rooms.Add(room);
-            //}
+        private bool CopyRoomToBoard(
+            BoardTile[,] board,
+            BoardTile[,] room,
+            Point topLeft)
+        {
+            if (topLeft.X + room.GetLength(0) > board.GetLength(0))
+            {
+                return false;
+            }
 
-            return board;
+            if (topLeft.Y + room.GetLength(1) > board.GetLength(1))
+            {
+                return false;
+            }
+
+            for (int x = topLeft.X; x < topLeft.X + room.GetLength(0); x++)
+            {
+                for (int y = topLeft.Y; y < topLeft.Y + room.GetLength(1); y++)
+                {
+                    board[x, y] = room[x - topLeft.X, y - topLeft.Y];
+                }
+            }
+
+            return true;
+        }
+
+        private bool CanRoomFit(
+            BoardTile[,] board,
+            BoardTile[,] room,
+            Point topLeft)
+        {
+            if(topLeft.X + room.GetLength(0) > board.GetLength(0))
+            {
+                return false;
+            }
+
+            if (topLeft.Y + room.GetLength(1) > board.GetLength(1))
+            {
+                return false;
+            }
+
+            for (int x = topLeft.X; x < topLeft.X + room.GetLength(0); x++)
+            {
+                for (int y = topLeft.Y; y < topLeft.Y + board.GetLength(1); y++)
+                {
+                    if(board[x, y] != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -10,24 +10,31 @@ var roomGenerator = new RoomGenerator(
     new RoomValidator(),
     new ArrayTrimmer<BoardTile>(),
     new DoorInserter(randomiser, new TilePicker()));
+var dungeonGenerator = new DungeonGenerator(
+    randomiser,
+    roomGenerator,
+    new ArrayTrimmer<BoardTile>());
 
-var options = new RoomGeneratorOptions
+var options = new DungeonGeneratorOptions
 {
-    Width = new IntRange(5, 10),
-    Height = new IntRange(5, 10),
-    Parts = new IntRange(2, 5),
-    PartWidth = new IntRange(3, 5),
-    PartHeight = new IntRange(3, 5),
+    Width = 20,
+    Height = 20,
+    Rooms = new IntRange(2,10),
+    RoomWidth = new IntRange(3, 5),
+    RoomHeight = new IntRange(3, 5),
+    RoomParts = new IntRange(2, 5),
+    RoomPartWidth = new IntRange(3, 5),
+    RoomPartHeight = new IntRange(3, 5),
     DoorSize = new IntRange(1, 2),
-    DoorCount = new IntRange(1, 3)
+    RoomDoorCount = new IntRange(1, 3),
 };
 
 var more = true;
 while (more)
 {
     Console.Clear();
-    var room = roomGenerator.Generate(options);
-    var formatted = room.ToFormattedString(false);
+    var dungeon = dungeonGenerator.Generate(options);
+    var formatted = dungeon.ToFormattedString(false);
     Console.WriteLine(formatted);
     Console.WriteLine("More? (y,n)");
     more = Console.ReadKey(true).Key == ConsoleKey.Y;
